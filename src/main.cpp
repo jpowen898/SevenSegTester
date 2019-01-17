a8bf0a4f5d3f251bca90b58f4ac6a00cb2c6dedc
Parker <plowen12@gmail.com>
2019-01-17 13:07:03 -0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,22 @@
 #include "ShiftOut.h"
 
 int main() {
-    DigitalOut led(D0);
+    DigitalOut led(D6);
     // put your setup code here, to run once:
     //SevenSegment test(D14, D15);
     //ShiftReg test(D14, D15);
-    ShiftOut test(D0, D1, D2, NC, NC);
+    ShiftOut test(D5, D14, D15, NC, NC);
     while(1) {
         //test.on();
+        led = 0;
         test.writeBit(1);
         for (int i = 0; i < 16; i++){
-            //wait(.5);
+            wait(.5);
+            test.writeBit(1);
+            led = 1 ^ led;
+            wait(.5);
             test.writeBit(0);
+            led = 1 ^ led;
         }
         //wait(.5);
         //test.off();
